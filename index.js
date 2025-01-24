6d2cdb3c474ee1a2a45a5662589e79f9bfd97de9
andymaru <anderson.nogueira01@hotmail.com>
2025-01-23 22:34:01 -0400

    [NEW] Create entrada e Saída Pop Up

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
-const para = document.querySelector("button");
+const openPopupButton = document.getElementById('openPopup');
 
-para.addEventListener("click", atualizarNome);
+const closePopupButton = document.getElementById('closePopup');
 
-function atualizarNome() {
-  var nome = prompt("Insira um novo nome");
-  para.textContent = "Jogador 1: " + nome;
-}
+const popup = document.getElementById('popup');
+
+const overlay = document.getElementById('overlay');
+
+//Abre o Pop-Up
+openPopupButton.addEventListener('click', () => {
+  popup.style.display = 'block';
+  overlay.style.display = 'block';
+});
+
+//fecha o Pop-Up
+closePopupButton.addEventListener('click', () =>  {
+  popup.style.display ='none';
+  overlay.style.display = 'none';
+});
+
+//Fecha o pop-up ao clicar no fundo escurecido
+overlay.addEventListener('click', () => {
+  popup.style.display = 'none';
+  overlay.style.display = 'none'
+});
